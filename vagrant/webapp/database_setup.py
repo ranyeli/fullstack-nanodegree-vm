3caeb7e938d5aaa825087fd67bae24d3ffd779a5
ranyeli <robert.ranyeli@gmail.com>
2017-01-28 16:57:22 -0400

    add database setup script

---
+++ vagrant/webapp/database_setup.py
@@ -0,0 +1,28 @@
+import sys
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from  sqlalchemy.orm import relationship
+from  sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Restaurant(Base):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+
+    __tablename__ = 'restaurant'
+
+class MenuItem(Base):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+    restaurant_id = Column(ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
+
+    __tablename__ = 'menu_item'
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.create_all(engine)
