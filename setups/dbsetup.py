14468d58234ba786fc26b45a69b73805758abcfe
ranyeli <robert.ranyeli@gmail.com>
2017-02-06 19:02:48 -0400

    change connection string to hobby dev

--- setups/dbsetup.py
+++ setups/dbsetup.py
@@ -7,5 +7,5 @@ import models.restaurant
 import models.course
 
 
-engine = create_engine('postgresql://postgres@localhost/restaurantmenu')
+engine = create_engine('postgres://wbdchxpdhwsviw:5a5e6f7070ff650e9620a3c00533167944d8b516f826b9effebdefab69207008@ec2-54-163-234-4.compute-1.amazonaws.com:5432/df0u3r9mjc3vbs')
 Base.metadata.create_all(engine)
