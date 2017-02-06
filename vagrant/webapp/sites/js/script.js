f57aa596bec1ed998a46a14be0a0fd7e9b38b646
ranyeli <robert.ranyeli@gmail.com>
2017-02-06 12:34:17 -0400

    delete unused files

--- vagrant/webapp/sites/js/script.js
+++
@@ -1,9 +0,0 @@
-window.onload = init;
-
-function init(){
-    var rests = document.querySelector('#rests')
-    var delete = function(e){
-        alert(e.target + ' ' + e.currentTarget);
-    };
-    rests.addEventListener("click", delete, false)
-}
