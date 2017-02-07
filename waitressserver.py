8a9d08806837d1cdb9a977c944d8a0fc8a0a06cc
ranyeli <robert.ranyeli@gmail.com>
2017-02-06 23:29:57 -0400

    change secret key

--- waitressserver.py
+++ waitressserver.py
@@ -1,8 +1,9 @@
 from waitress import serve
 from finalproject import app
 
+
 if __name__ == '__main__':
-    app.secret_key = '!@#$%^&*()_+`sdft'
+    app.secret_key = 'thisIsMySecretKey'
     app.debug = True
     serve(app, listen='*:80')
 
